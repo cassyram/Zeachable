ad900858a5bf82bf9da459151e9d3f2e398cf683
cassandraram91 <cassandraram91@gmail.com>
2022-03-28 17:55:22 -0400

    Added utils classes

---
+++ src/test/java/com/takehome/utils/BrowserUtils.java
@@ -0,0 +1,25 @@
+package com.takehome.utils;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.NoSuchElementException;
+
+public class BrowserUtils {
+
+    public static void checkIfElementVisible(WebElement element){
+
+        try {
+            Assert.assertTrue("Element not visible: " + element, element.isDisplayed());
+        } catch (NoSuchElementException e) {
+            Assert.fail("Element not found: " + element);
+
+        }
+    }
+
+    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeToWaitInSec);
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+}
---
+++ src/test/java/com/takehome/utils/ConfigurationReader.java
@@ -0,0 +1,24 @@
+package com.takehome.utils;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties = new Properties();
+
+    static{
+        try {
+            FileInputStream inputStream = new FileInputStream("configuration.properties");
+            properties.load(inputStream);
+            inputStream.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Error occured while reading configuration file");
+        }
+    }
+
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
